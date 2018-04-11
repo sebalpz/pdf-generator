a6ecef2a1a59de393c9e1993c225b9356c7d2867
Seba <sdevries@schadenteam.net>
2018-04-11 13:08:08 +0200

    enhance data to one-string-shortcuts

--- src/Racklin/PdfGenerator/PdfGenerator.php
+++ src/Racklin/PdfGenerator/PdfGenerator.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Racklin\PdfGenerator;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * Class PdfGenerator
@@ -11,6 +12,8 @@ class PdfGenerator
 {
 
     protected $stEngine = null;
+    protected $defautFont;
+    protected $defaultFontSize;
 
     public function __construct()
     {
@@ -29,6 +32,7 @@ class PdfGenerator
      */
     public function generate($template, $data, $name = '', $desc = 'I') {
 
+        $templateDir = "";
         if(is_string($template) && is_file($template)) {
             $templateDir = dirname($template);
 
@@ -47,23 +51,55 @@ class PdfGenerator
 
             // set bacground image
             if (!empty($page['background'])) {
-                $img_file = $templateDir . DIRECTORY_SEPARATOR . $page['background'];
-                // get the current page break margin
-                $bMargin = $tcpdf->getBreakMargin();
-                // get current auto-page-break mode
-                $auto_page_break = $tcpdf->getAutoPageBreak();
-                // disable auto-page-break
-                $tcpdf->SetAutoPageBreak(false, 0);
-                // set bacground image
-                $tcpdf->Image($img_file, 0, 0, 210, 297, '', '', '', false, 300, '', false, false, 0);
-                // restore auto-page-break status
-                $tcpdf->SetAutoPageBreak($auto_page_break, $bMargin);
-                // set the starting point for the page content
-                $tcpdf->setPageMark();
+                $img_file = null;
+                if(is_file($templateDir . DIRECTORY_SEPARATOR . $page['background'])) {
+                    $img_file = $templateDir . DIRECTORY_SEPARATOR . $page['background'];
+                }
+                else if(is_file($page['background'])) {
+                    $img_file = $page['background'];
+                }
+                if(!is_null($img_file)) {
+                    if(mime_content_type($img_file) == 'application/pdf') {
+                        $tcpdf->setSourceFile($img_file);
+                        $tcpdf->tplId = $tcpdf->importPage(1);
+                        $tcpdf->useImportedPage($tcpdf->tplId, 0, 0, 210);
+                    }
+                    else if(strpos(mime_content_type($img_file),'image') !== false) {
+                        // get the current page break margin
+                        $bMargin = $tcpdf->getBreakMargin();
+                        // get current auto-page-break mode
+                        $auto_page_break = $tcpdf->getAutoPageBreak();
+                        // disable auto-page-break
+                        $tcpdf->SetAutoPageBreak(false, 0);
+                        // set bacground image
+                        $tcpdf->Image($img_file, 0, 0, 210, 297, '', '', '', false, 300, '', false, false, 0);
+                        // restore auto-page-break status
+                        $tcpdf->SetAutoPageBreak($auto_page_break, $bMargin);
+                        // set the starting point for the page content
+                        $tcpdf->setPageMark();
+                    }
+                }
 
             }
 
             foreach($page['data'] as $d) {
+                if(is_string($d)) {
+                    //shortcut
+                    $els = explode("|",$d);
+                    $d = [];
+                    if(sizeof($els) == 3) {
+                        $d['x'] = doubleval($els[0]);
+                        $d['y'] = doubleval($els[1]);
+                        $d['text'] = $els[2];
+                    }
+                    else {
+                        $d['x'] = 0;
+                        $d['y'] = 0;
+                        $d['text'] = $els[0];
+                    }
+                    $d['font'] = $this->defaultFont;
+                    $d['font-size'] = $this->defaultFontSize;
+                }
                 if (!empty($d['font']) && !empty($d['font-size'])) {
                     $tcpdf->SetFont($d['font'], '', $d['font-size'], '', true);
                 }
@@ -73,8 +109,8 @@ class PdfGenerator
                     $txt = $this->renderText($d['text'], $data);
                     $lines = explode("\n", $txt);
 
-                    $offsetY = ceil($d['font-size']/2.834 ?: 4);
-                    $y = (int) $d['y'];
+                    $offsetY = ceil($d['font-size'] / 2.834 ?: 4);
+                    $y = (int)$d['y'];
 
                     foreach ($lines as $line) {
                         $tcpdf->Text($d['x'], $y, $line);
@@ -86,15 +122,16 @@ class PdfGenerator
                 if (!empty($d['image'])) {
                     $img = $this->renderText($d['image'], $data);
 
-                    $tcpdf->Image($img, $d['x'], $d['y'], $d['w']?:0, $d['h']?:0, '', '', '', false, 300, '', false, false, 0);
+                    $tcpdf->Image($img, $d['x'], $d['y'], $d['w'] ?: 0, $d['h'] ?: 0, '', '', '', false, 300, '', false, false, 0);
                 }
 
                 // html
                 if (!empty($d['html'])) {
                     $html = $this->renderText($d['html'], $data);
                     $html = str_replace("\n", "<br/>", $html);
-                    $tcpdf->writeHTMLCell($d['w']?:0, $d['h']?:0, $d['x'], $d['y'], $html);
+                    $tcpdf->writeHTMLCell($d['w'] ?: 0, $d['h'] ?: 0, $d['x'], $d['y'], $html);
                 }
+
 
             }
         }
@@ -139,6 +176,12 @@ class PdfGenerator
         $tcpdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
         // set default font subsetting mode
         $tcpdf->setFontSubsetting(true);
+
+        if(isset($settings['info']['default-font'])) $this->defaultFont = $settings['info']['default-font'];
+        else $this->defaultFont = 'helvetica';
+
+        if(isset($settings['info']['default-font-size'])) $this->defaultFontSize = $settings['info']['default-font-size'];
+        else $this->defaultFontSize = 16;
 
         return $tcpdf;
     }
